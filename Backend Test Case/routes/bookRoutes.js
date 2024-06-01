16ce32568071f7dc89f6a5956209fa6c5bcdac22
Muhammad Fajri Al Basith <47000386+fajrialbasith@users.noreply.github.com>
2024-06-01 18:34:52 +0700

    Update bookRoutes.js

--- Backend Test Case/routes/bookRoutes.js
+++ Backend Test Case/routes/bookRoutes.js
@@ -1,7 +1,38 @@
 const express = require('express');
 const router = express.Router();
 const { getAllBooks } = require('../controllers/bookController');
+/**
+ * @swagger
+ * tags:
+ *   name: Books
+ *   description: Book management
+ */
 
+/**
+ * @swagger
+ * /books:
+ *   get:
+ *     summary: Retrieve a list of books
+ *     tags: [Books]
+ *     responses:
+ *       200:
+ *         description: A list of books
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   code:
+ *                     type: string
+ *                   title:
+ *                     type: string
+ *                   author:
+ *                     type: string
+ *                   stock:
+ *                     type: integer
+ */
 router.get('/', getAllBooks);
 
 module.exports = router;
