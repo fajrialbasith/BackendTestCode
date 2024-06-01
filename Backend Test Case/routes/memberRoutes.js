683ff09c2a84f4ab641bd3467947731e65b0ea90
Muhammad Fajri Al Basith <47000386+fajrialbasith@users.noreply.github.com>
2024-06-01 18:35:24 +0700

    Update memberRoutes.js

--- Backend Test Case/routes/memberRoutes.js
+++ Backend Test Case/routes/memberRoutes.js
@@ -2,6 +2,36 @@ const express = require('express');
 const router = express.Router();
 const { getAllMembers } = require('../controllers/memberController');
 
+/**
+ * @swagger
+ * tags:
+ *   name: Members
+ *   description: Member management
+ */
+
+/**
+ * @swagger
+ * /members:
+ *   get:
+ *     summary: Retrieve a list of members
+ *     tags: [Members]
+ *     responses:
+ *       200:
+ *         description: A list of members
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   code:
+ *                     type: string
+ *                   name:
+ *                     type: string
+ */
+
+
 router.get('/', getAllMembers);
 
 module.exports = router;
